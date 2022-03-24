aadacd8f2165dd64e4fd6851526c33c97c5b8b43
lenivyj <taxitaxi621@gmail.com>
2022-03-24 20:14:13 +0600

    dz1

--- script2.js
+++ script2.js
@@ -2,18 +2,18 @@
 let a;
 while (a != 99) {
     k = k + 1;
-    a = (prompt('Введите число'));
+    a = prompt('Введите число');
 }
 alert(('кол-во чисел - ') + (k));*/
 
 let b = 0;
 let s = 0;
 let y;
-y = (prompt('Введите число'));
+y = Number(prompt('Введите число'));
 while (y != 999) {
     b = b + 1;
     s = s += y;
-    y = (prompt('Введите число'));
+    y = Number(prompt('Введите число'));
 }
-alert(('кол-во чисел - ') + String(b));
-alert(('сумма = ') + String(s));
+alert(('кол-во чисел - ') + (b));
+alert(('сумма = ') + (s));
